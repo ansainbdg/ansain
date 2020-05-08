261b302a293839b199b7e9af6af2b433b7b03bf1
ansain <tit1campus@hotmail.com>
2020-05-08 13:58:32 +0900

    ブランチのテスト、初回作成

---
+++ m_obj_lesson01.py
@@ -0,0 +1,37 @@
+import math
+
+
+def manager(base, position):
+    ins = round((base+position)*0.15, -1)
+    income = (base+position-ins)*0.9
+    return income
+
+
+def general(base, overtime):
+    money = math.floor(base*(1+overtime*1.25/160)/10)*10
+    ins = round(money*0.15, -1)
+    income = (money-ins)*0.9
+    return income
+
+
+def part(unit, time):
+    income = unit*time*0.9
+    return income
+
+
+if __name__ == "__main__":
+    terao_income = manager(350000, 80000)
+    wakabayashi_income = manager(375000, 40000)
+    terada_income = general(320000, 30)
+    hirota_income = general(295000, 20)
+    kannuma_income = general(220000, 35)
+    kikuchi_income = part(1200, 90)
+    yamagishi_income = part(1000, 120)
+
+    print(f' 寺尾さんの振込額は{terao_income}')
+    print(f' 若林さんの振込額は{wakabayashi_income}')
+    print(f' 寺田さんの振込額は{terada_income}')
+    print(f' 広田さんの振込額は{hirota_income}')
+    print(f' 菅沼さんの振込額は{kannuma_income}')
+    print(f' 菊池さんの振込額は{kikuchi_income}')
+    print(f' 山岸さんの振込額は{yamagishi_income}')
