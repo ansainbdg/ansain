740ebefcb6dd7cfdc30d200bafab0d04305e2fdf
ansain <tit1campus@hotmail.com>
2020-05-08 16:12:10 +0900

    アルバイト追加

--- branch_test.py
+++ branch_test.py
@@ -14,5 +14,16 @@ def trunc10(v):
     t = int(v / 10) * 10
     return t
 
+# アルバイト
+def calc_part(unit, time):
+    salary = unit * time
+    tax = salary * tax_rate
+    bank = salary - tax
+    return int(bank)
+
 if __name__ == "__main__":
-    pass
+    bank_kiku = calc_part(1200, 90)
+    bank_yama = calc_part(1000, 120)
+
+    print(f'菊地章さんの振込額={bank_kiku}')
+    print(f'山岸柑奈さんの振込額={bank_yama}')
